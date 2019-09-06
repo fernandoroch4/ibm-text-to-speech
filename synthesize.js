166a64f7dd1a9bf3d386c4988559e8e84a9aa522
Fernando Rocha <fernandoroch4@gmail.com>
2019-09-06 00:46:06 -0300

    Inittial commit

---
+++ synthesize.js
@@ -0,0 +1,29 @@
+const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
+const config = require('./config');
+const path = require('path');
+const fs = require('fs');
+
+const textToSpeech = new TextToSpeechV1({
+  iam_apikey: config.apiKey,
+  url: config.url
+});
+
+module.exports = text => {
+  return new Promise((resolve, reject) => {
+    const synthesizeParams = {
+      text: text,
+      accept: 'audio/wav',
+      voice: config.voice
+    };
+  
+    textToSpeech.synthesize(synthesizeParams)
+      .then(audio => {
+        let stream = audio.pipe(fs.createWriteStream(__dirname + '/public/text.wav'));
+        stream.on('finish', () => resolve());
+      })
+      .catch(err => {
+        console.error('error:', err);
+        reject(err);
+      });
+  });
+};
